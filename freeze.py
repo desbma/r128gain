6c44d20194e0a3a7b54a16de5e502b821a7a3d6f
desbma <desbma@users.noreply.github.com>
2020-01-12 13:37:09 +0100

    Windows freeze: Don't include tkinter

--- freeze.py
+++ freeze.py
@@ -9,7 +9,8 @@ from cx_Freeze import setup, Executable
 with open(os.path.join("r128gain", "__init__.py"), "rt") as f:
   version = re.search("__version__ = \"([^\"]+)\"", f.read()).group(1)
 
-build_exe_options = {"optimize": 0}
+build_exe_options = {"optimize": 0,
+                     "excludes": ["tkinter"]}
 
 setup(name="r128gain",
       version=version,
