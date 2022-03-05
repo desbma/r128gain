6d3153c64a99af2128b438adeba18e46446597a0
desbma <desbma@users.noreply.github.com>
2022-03-05 11:03:46 +0100

    chore: officially support Python 3.9 & 3.10

--- setup.py
+++ setup.py
@@ -66,6 +66,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Topic :: Utilities",
     ],
